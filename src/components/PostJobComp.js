30d24976ca1d07cbdbf660166bbb18fc3d863701
rajeshprofile <hirajesh.official@gmail.com>
2021-12-25 19:45:25 +0530

    bug fixes

--- src/components/PostJobComp.js
+++ src/components/PostJobComp.js
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react/cjs/react.development';
 import { authenticate, createJob } from '../Auth/auth';
 
 import "./PostJobComp.scss";
